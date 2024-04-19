bd62fbdcffe56d48f9f867074ac7726f113c79da
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2024-04-19 17:54:31 -0300

    questap 9

---
+++ 1Periodo/ALGORITMOS_E_PROGRAMACAO/revisao/exercicio9.c
@@ -0,0 +1,10 @@
+/*Implemente um alguritmo que receba um número inteiro e imprima seu sucessor e o
+antecessor de um número*/
+#include <stdio.h>
+int numero;
+
+int main() {
+    printf("Insira um número: \n");
+    scanf("%d")
+}
+
