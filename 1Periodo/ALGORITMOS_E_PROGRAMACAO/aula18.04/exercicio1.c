24845bc01eaaca33827cc100ceaebce251d695f2
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2024-04-19 16:00:11 -0300

    revisao

--- 1Periodo/ALGORITMOS_E_PROGRAMACAO/aula18.04/exercicio1.c
+++ 1Periodo/ALGORITMOS_E_PROGRAMACAO/aula18.04/exercicio1.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+float ProdutoA, ProdutoB, ProdutoC, SomaAB, SomaBC;
+
+int main() {
+
+    printf("Digite o valor do Produto A.\n");
+    scanf("%f", &ProdutoA);
+    printf("Digite o valor do Produto B.\n");
+    scanf("%f", &ProdutoB);
+    printf("Digite o valor do Produto C.\n");
+    scanf("%f", &ProdutoC);
+    SomaAB = ProdutoA + ProdutoB;
+    SomaBC = ProdutoB + ProdutoC;
+}
